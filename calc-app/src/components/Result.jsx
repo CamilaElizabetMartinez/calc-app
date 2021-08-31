d61715df65cfbc050cd046fbac5bc948eb857f16
camila martinez <martinezce_@hotmail.com>
2021-08-31 19:04:18 +0300

    como mostrar contenido JS

--- calc-app/src/components/Result.jsx
+++ calc-app/src/components/Result.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Result = (props) => {
     return(
         <div className="result">
-            <span>0</span>
+            {props.value}
         </div>
     )
 }
