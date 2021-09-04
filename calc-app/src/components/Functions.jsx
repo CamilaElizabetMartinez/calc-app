4236e71192bed9f9a1e3e79792dad69117b4832f
camila martinez <martinezce_@hotmail.com>
2021-09-04 16:56:57 +0300

    refactor(Functions): añadimos propiedad al boton

--- calc-app/src/components/Functions.jsx
+++ calc-app/src/components/Functions.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import Button from './Button';
-import './App.css'
 
 const Functions = ({onContenClear, onDelete}) => (
   <section className="functions">
-    <Button text="clear" clickHandler={onContenClear}></Button>
-    <Button text="&larr;" clickHandler={onDelete}></Button>
-
+    <Button type="button-long-text" text="clear" clickHandler={onContenClear}/>
+    <Button type="" text="&larr;" clickHandler={onDelete}/>
   </section>
 )
 
-Functions.propTypes ={
+Functions.propTypes = {
   onContenClear: propTypes.func.isRequired,
   onDelete: propTypes.func.isRequired
 }
-export default Functions
+export default Functions 
