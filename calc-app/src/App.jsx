ccb51fd0a5740bf48328626efb0e794d99b6c689
camila martinez <martinezce_@hotmail.com>
2021-09-04 11:46:12 +0300

    refactor(App): importamos Functions

--- calc-app/src/App.jsx
+++ calc-app/src/App.jsx
@@ -2,22 +2,19 @@ import React from "react";
 import Result from "./components/Result";
 import Button from "./components/Button";
 import MathOperations from "./components/MathOperations";
+import Functions from "./components/Functions";
 import "./App.css";
 
 //Funcion de flecha
 const App = () => {
-
-const clickHandlerFunction = text => {
-
-}
+  const clickHandlerFunction = (text) => {};
 
   //Lo que ejecuta la funcion
   return (
     <main className="react-calculator">
       <Result value={undefined} />
       <div className="numbers">
-        <Button text="1" clickHandler={(clickHandlerFunction) => { 
-        }}/>
+        <Button text="1" clickHandler={(clickHandlerFunction) => {}} />
         <button>2</button>
         <button>3</button>
         <button>4</button>
@@ -26,14 +23,18 @@ const clickHandlerFunction = text => {
         <button>7</button>
         <button>8</button>
         <button>9</button>
+        <button>0</button>
       </div>
 
-      <div className="functions">
-        <button>clear</button>
-        <button>r</button>
-      </div>
+      <Functions
+        onContenClear={() => console.log("Content clear")}
+        onDelete={() => console.log("onDelete")}
+      />
 
-    <MathOperations/>
+      <MathOperations
+        onClickOperation={(operation) => console.log("Operation:", operation)}
+        onClickEqual={(equal) => console.log("Equal:", equal)}
+      />
     </main>
   );
 };
