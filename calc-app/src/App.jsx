0926d21addc053daa716268c560d52d502d2f5e0
camila martinez <martinezce_@hotmail.com>
2021-09-07 12:16:38 +0300

    refactor(App): añadimos condicional para validar que stack sea mayor a 0

--- calc-app/src/App.jsx
+++ calc-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Functions from "./components/Functions";
 import Numbers from "./components/Numbers";
 import MathOperations from "./components/MathOperations";
@@ -14,24 +14,40 @@ const App = () => {
   //2da posicion: funcion que me va a permitit modificar el valor por defecto
 
   //Lo que ejecuta la funcion
-  console.log("Renderizacion de App")
+  console.log("Renderizacion de App");
   return (
     <main className="react-calculator">
       <Result value={stack} />
       <Numbers
-        onClickNumber={number => {console.log("Click en number", number)
-        setStack (number)
-      }}
+        onClickNumber={(number) => {
+          console.log("Click en number", number);
+          setStack(`${stack}${number}`);
+        }}
       />
 
       <Functions
-        onContenClear={() => console.log("Content clear")}
-        onDelete={() => console.log("onDelete")}
+        onContenClear={() => {
+          console.log("Content clear");
+          setStack("");
+        }}
+        onDelete={() => {
+          if (stack.length > 0) {
+            const newStack = stack.substring(0, stack.length - 1);
+            console.log("onDelete", newStack);
+            setStack(newStack);
+          }
+        }}
       />
 
       <MathOperations
-        onClickOperation={(operation) => console.log("Operation:", operation)}
-        onClickEqual={(equal) => console.log("Equal:", equal)}
+        onClickOperation={(operation) => {
+          console.log("Operation:", operation);
+          setStack(`${stack}${operation}`);
+        }}
+        onClickEqual={(equal) => {
+          console.log("Equal:", equal);
+          setStack(`${stack}${equal}`);
+        }}
       />
     </main>
   );
